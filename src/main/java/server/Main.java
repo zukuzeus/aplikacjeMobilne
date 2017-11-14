7a853e4e79ce0f40a4036d24295515bf65662f61
zukuzeus <danielzukowskii@gmail.com>
2017-11-14 22:43:59 +0100

    poprawka aby endpointy zwracały wynik tylko jak haslo i uzytkownik sie zgadza za kazdym razem z kazdym zapytaniem

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -20,26 +20,26 @@ public class Main {
         post("/results", (req, res) -> {
             if (isLoginAndPasswordMatch(req)) {
                 return CRUD.QUERY.getResultsAsProductService(req.queryParams("username")).getAllProducts();
-            } else return false;
+            } else return "not autenticated";
 
         }, JsonUtil.json());
 
         post("/delete", (req, res) -> {
             if (isLoginAndPasswordMatch(req)) {
                 return CRUD.DELETE.deleteProduct(req.queryParams("username"), req.queryParams("product"));
-            } else return false;
+            } else return "not autenticated";
         }, JsonUtil.json());
 
         post("/update", (req, res) -> {
             if (isLoginAndPasswordMatch(req)) {
                 return CRUD.UPDATE.updateProductQuantity(req.queryParams("username"), req.queryParams("product"), Integer.parseInt(req.queryParams("quantity")));
-            } else return false;
+            } else return "not autenticated";
         }, JsonUtil.json());
 
         post("/add", (req, res) -> {
             if (isLoginAndPasswordMatch(req)) {
                 return CRUD.INSERT.insertProduct(req.queryParams("username"), req.queryParams("product"), req.queryParams("shop"), Double.parseDouble(req.queryParams("price")), 0);
-            } else return false;
+            } else return "not autenticated";
         }, JsonUtil.json());
 
         after((req, res) -> {
